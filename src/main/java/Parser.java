6b891906a9ec43ada40f23925f3abdacc4d8eec8
varbandim <varbandim@yandex.ru>
2018-04-07 10:46:02 +0300

    Определил колонку, в которой будет номенклатура у исходного файла

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -22,31 +22,48 @@ public class Parser {
             e.printStackTrace();
         }
 
+        String tmp = "";
+        int workCol = 0;
         Sheet sheet = wb.getSheetAt(0);//открыл книгу
-        Iterator<Row> it = sheet.iterator();//начинаю обходить книгу построчно
-        while (it.hasNext()) {
-            Row row = it.next();
-            Iterator<Cell> cells = row.iterator();
-            while (cells.hasNext()) {
-                Cell cell = cells.next();
-                int cellType = cell.getCellType();
-                if (cellType == Cell.CELL_TYPE_STRING) {
-                    result = cell.getStringCellValue().split("\\s+");
-                }
-                cell.setCellValue("Пупс");//тут должно быть сравнение названия номенклатуры и сравнение цен
-                try (FileOutputStream out = new FileOutputStream(new File(name))) {
-                    wb.write(out);
-                } catch (IOException e) {
-                    e.printStackTrace();
+        //Прочитать первую строку
+        int rowFirst = sheet.getFirstRowNum();
+        Row firstRowString = sheet.getRow(rowFirst);
+        short minCol = firstRowString.getFirstCellNum();
+        short maxCol = firstRowString.getLastCellNum();
+        for (short i = minCol; i < maxCol; i++) {
+            Cell cell = firstRowString.getCell(i);
+            int cellType = cell.getCellType();
+            if (cellType == Cell.CELL_TYPE_STRING) {
+                tmp = cell.getStringCellValue();
+                if ((tmp.compareToIgnoreCase("Номенклатура") == 0) /*добавить сравнение на "Товар"*/) {
+                    workCol = cell.getColumnIndex();
                 }
             }
         }
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+//        Iterator<Row> it = sheet.iterator();//начинаю обходить книгу построчно
+//        while (it.hasNext()) {
+//            Row row = it.next();
+//            Iterator<Cell> cells = row.iterator();
+//            while (cells.hasNext()) {
+//                Cell cell = cells.next();
+//                int cellType = cell.getCellType();
+//                if (cellType == Cell.CELL_TYPE_STRING) {
+//                    result = cell.getStringCellValue().split("\\s+");
+//                }
+//                cell.setCellValue("Пупс");//тут должно быть сравнение названия номенклатуры и сравнение цен
+//                try (FileOutputStream out = new FileOutputStream(new File(name))) {
+//                    wb.write(out);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        try {
+//            in.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
         return "";
     }
 }
