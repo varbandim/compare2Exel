b39ef80ddadba24a8f8ad5568724d5c25cdb294d
varbandim <varbandim@yandex.ru>
2018-04-07 18:21:43 +0300

    Прочитал номенклатуру и разбил по словам

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Parser.parse("Прайс для теста-1.xls"));
+        System.out.println(Parser.parse("Прайс для теста-1.xls", "Прайс для теста-2.xls"));
     }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,14 +8,21 @@ import java.util.Iterator;
 
 public class Parser {
 
-    public static String parse(String name) {
+    public static String parse(String name1, String name2) {
 
-        String[] result = null;
-        InputStream in = null;
-        HSSFWorkbook wb = null;
+        String[] stringFirstFile = null;
+        String[] stringSecondFile = null;
+        InputStream in1 = null;
+        InputStream in2 = null;
+        HSSFWorkbook wbFirstFile = null;
+        HSSFWorkbook wbSecondFileFile = null;
+        int workCol = 0;
+
         try {
-            in = new FileInputStream(name);
-            wb = new HSSFWorkbook(in);
+            in1 = new FileInputStream(name1);
+            in2 = new FileInputStream(name2);
+            wbFirstFile = new HSSFWorkbook(in1);
+            wbSecondFileFile = new HSSFWorkbook(in2);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -23,15 +30,16 @@ public class Parser {
         }
 
         String tmp = "";
-        int workCol = 0;
-        Sheet sheet = wb.getSheetAt(0);//открыл книгу
-        //Прочитать первую строку
-        int rowFirst = sheet.getFirstRowNum();
-        Row firstRowString = sheet.getRow(rowFirst);
-        short minCol = firstRowString.getFirstCellNum();
-        short maxCol = firstRowString.getLastCellNum();
+        Sheet sheet1 = wbFirstFile.getSheetAt(0);//открыл файл 1
+        Sheet sheet2 = wbSecondFileFile.getSheetAt(0);//открыл файл 2
+        //Прочитать первую строку и определить рабочую колнку
+        int rowFirst = sheet1.getFirstRowNum();
+        int rowLast = sheet1.getLastRowNum();
+        Row firstRow = sheet1.getRow(rowFirst);
+        short minCol = firstRow.getFirstCellNum();
+        short maxCol = firstRow.getLastCellNum();
         for (short i = minCol; i < maxCol; i++) {
-            Cell cell = firstRowString.getCell(i);
+            Cell cell = firstRow.getCell(i);
             int cellType = cell.getCellType();
             if (cellType == Cell.CELL_TYPE_STRING) {
                 tmp = cell.getStringCellValue();
@@ -40,30 +48,24 @@ public class Parser {
                 }
             }
         }
-//        Iterator<Row> it = sheet.iterator();//начинаю обходить книгу построчно
-//        while (it.hasNext()) {
-//            Row row = it.next();
-//            Iterator<Cell> cells = row.iterator();
-//            while (cells.hasNext()) {
-//                Cell cell = cells.next();
-//                int cellType = cell.getCellType();
-//                if (cellType == Cell.CELL_TYPE_STRING) {
-//                    result = cell.getStringCellValue().split("\\s+");
-//                }
-//                cell.setCellValue("Пупс");//тут должно быть сравнение названия номенклатуры и сравнение цен
-//                try (FileOutputStream out = new FileOutputStream(new File(name))) {
-//                    wb.write(out);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        try {
-//            in.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
+        for (int i = ++rowFirst; i < rowLast; i++) {
+            Row row = sheet1.getRow(i);
+            Cell cell = row.getCell(workCol);
+            stringFirstFile = cell.getStringCellValue().split("\\s+");
+            //открываем второй файл и читаем строки, ищем столбец "Номенклатура"
+        }
+
+        //Прочитать строку из рабочей колонки и разбить на слова
+            //тестовый вывод
+            for (String s :
+                    stringFirstFile) {
+                System.out.println(s);
+            }
+        }
+
+
+
+
         return "";
     }
 }
